c3827bf25d12218ea032dd499a566c726a2b73b7
minhkhoi1605 <87912060+minhkhoi1605@users.noreply.github.com>
2021-07-25 02:33:32 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -2,15 +2,15 @@ const textConfig = {
   text1: "He luu cậu!",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
   text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
+  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm ny tớ rùi đó nha :v",
   text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
+  text6: "Tâm sự vào ib cho tớ đi <3",
+  text7: "Chuyện gì làm cậu buồn thế ? :vvvv",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text9: " Tớ hơi khó nói ấy. Ngại nma thôi tớ sẽ kể <3",
+  text10: "Tớ biết mà ^^ Thoải mái lên nào. Cậu mà xem rồi thì mess tớ đã có tin rồi. Đừng có lươn vs tớ ",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Tối mai mình nói chuyện với nhau đi. Đừng chán mãi nữa nhaaa ",
   text12: "Okii lunn <3",
 };
 
